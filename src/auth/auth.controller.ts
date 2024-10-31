3975024edd9bc931cd8a0e08616eff98d059f412
미나 이 <chsjen492@gmail.com>
2024-10-31 14:03:48 +0900

    Refactor: ORM migration
    TypeORM을 Prisma로 변경하였습니다.
    - entity 코드 삭제
    - 코드 변경에 따라 기존 코드 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Req, Res, Session, UseGuards } from '@nestjs/common'
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common'
 import { Request, Response } from 'express'
 import { GoogleAuthGuard } from './auth.guard'
 
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { UserService } from './user.service'
 import { UserController } from './user.controller'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { User } from './user.entity'
+import { PrismaModule } from '../../prisma/prisma.module'
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
+    imports: [PrismaModule],
     providers: [UserService],
     controllers: [UserController],
     exports: [UserService],

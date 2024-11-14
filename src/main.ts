2c1de79ea2101a0011e76aef661e8e6f936b9d74
미나 이 <chsjen492@gmail.com>
2024-11-14 11:34:55 +0900

    Feat: add morgan to log response

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { setUpSession } from './session/init.session'
+import * as morgan from 'morgan'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
@@ -12,6 +13,21 @@ async function bootstrap() {
         allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
         exposedHeaders: ['set-cookie'],
     })
+
+    morgan.token('header', (req, res, param: string | number | boolean) => {
+        let headerName: string
+        if (typeof param === 'string') headerName = param
+        else if (typeof param === 'number') headerName = param.toString()
+        else headerName = String(param)
+
+        const value = res.getHeader(headerName)
+        if (typeof value === 'string') return value
+        if (typeof value === 'number') return value.toString()
+        if (Array.isArray(value)) return value.join(', ')
+        return ''
+    })
+
+    app.use(morgan(':method :url :status :res[content-length] - :response-time ms :header[set-cookie]'))
     const config = new DocumentBuilder().setTitle('Test').setDescription('swagger API documentation test').setVersion('1.0').build()
     await setUpSession(app)
 
